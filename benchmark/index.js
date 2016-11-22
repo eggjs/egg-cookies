17b875cf1813654af644a826df02a198f14bd756
dead-horse <dead_horse@qq.com>
2016-11-22 12:28:16 +0800

    test: add benchmark test for get

--- benchmark/index.js
+++ benchmark/index.js
@@ -11,8 +11,21 @@ const keys = [ 'this is a very very loooooooooooooooooooooooooooooooooooooong ke
 const eggCookie = createEggCookie();
 const cookie = createCookie();
 
-eggCookie.set('foo', 'bar', {});
-cookie.set('foo', 'bar', {});
+console.log('------------get test----------');
+console.log(eggCookie.get('eggSign', { signed: true }));
+console.log(eggCookie.get('eggSign'));
+console.log(eggCookie.get('eggEncrypt', { encrypt: true }));
+console.log(cookie.get('sign', { signed: true }));
+console.log(cookie.get('sign'));
+
+console.log('------------set test----------');
+eggCookie.set('eggSign', 'egg signed cookie', { signed: true });
+cookie.set('sign', 'signed cookie', { signed: true });
+eggCookie.set('eggEncrypt', 'egg encrypt cookie', { encrypt: true });
+console.log(eggCookie.ctx.response.headers);
+console.log(cookie.response.headers);
+
+console.log('------------benchmark start----------');
 
 suite
 .add('create EggCookie', function() {
@@ -36,6 +49,21 @@ suite
 .add('EggCookies.set with encrypt', function() {
   createEggCookie().set('foo', 'bar', { encrypt: true });
 })
+.add('EggCookies.get with signed', function() {
+  createEggCookie().get('eggSign', { signed: true });
+})
+.add('Cookies.get with signed', function() {
+  createCookie().get('sign', { signed: true });
+})
+.add('EggCookies.get without signed', function() {
+  createEggCookie().get('eggSign', { signed: false });
+})
+.add('Cookies.get without signed', function() {
+  createEggCookie().get('sign', { signed: false });
+})
+.add('EggCookies.get with encrypt', function() {
+  createEggCookie().get('eggEncrypt', { encrypt: false });
+})
 .on('cycle', event => benchmarks.add(event.target))
 .on('start', () => console.log('\n  node version: %s, date: %s\n  Starting...', process.version, Date()))
 .on('complete', () => {
@@ -46,7 +74,7 @@ suite
 
 function createCtx(egg) {
   const request = {
-    headers: { cookie: 'foo=bar;' },
+    headers: { cookie: 'eggSign=egg signed cookie; eggSign.sig=SQ4wyGWr8vhSg7XCiz_MSxpHQ2GImbxE24fg4JVz7-o; sign=signed cookie; sign.sig=PvhhL9qTxML8uYSOaG_4Fr6EIEE; eggEncrypt=EpfmKzY4tX5OhafZS-onWOEIL0-CR6N_uGkFUFDCUno=;' },
     get(key) {
       return this.headers[key.toLowerCase()];
     },
@@ -80,7 +108,7 @@ function createCtx(egg) {
       this.response.set(key, value);
     },
     get(key) {
-      this.request.get(key);
+      return this.request.get(key);
     },
   };
 }
@@ -93,10 +121,15 @@ function createCookie() {
   return new Cookies(ctx.req, ctx.res, { keys });
 }
 
-// create EggCookie               x 6,122,689 ops/sec ±1.94% (84 runs sampled)
-// create Cookie                  x   915,465 ops/sec ±2.64% (79 runs sampled)
-// EggCookies.set with signed     x   105,077 ops/sec ±0.84% (86 runs sampled)
-// Cookies.set with signed        x    69,956 ops/sec ±2.21% (80 runs sampled)
-// EggCookies.set without signed  x   477,852 ops/sec ±1.04% (87 runs sampled)
-// Cookies.set without signed     x   355,754 ops/sec ±1.96% (83 runs sampled)
-// EggCookies.set with encrypt    x    88,396 ops/sec ±1.19% (83 runs sampled)
+// create EggCookie              x 6,251,382 ops/sec ±0.87% (86 runs sampled)
+// create Cookie                 x   904,067 ops/sec ±2.36% (78 runs sampled)
+// EggCookies.set with signed    x    92,009 ops/sec ±1.16% (86 runs sampled)
+// Cookies.set with signed       x    67,426 ops/sec ±2.37% (83 runs sampled)
+// EggCookies.set without signed x   455,434 ops/sec ±0.77% (87 runs sampled)
+// Cookies.set without signed    x   341,823 ops/sec ±2.54% (78 runs sampled)
+// EggCookies.set with encrypt   x    83,106 ops/sec ±1.17% (81 runs sampled)
+// EggCookies.get with signed    x    85,097 ops/sec ±1.53% (85 runs sampled)
+// Cookies.get with signed       x    87,334 ops/sec ±2.92% (77 runs sampled)
+// EggCookies.get without signed x 2,120,445 ops/sec ±1.08% (86 runs sampled)
+// Cookies.get without signed    x 1,988,503 ops/sec ±1.41% (85 runs sampled)
+// EggCookies.get with encrypt   x   749,463 ops/sec ±1.26% (83 runs sampled)
