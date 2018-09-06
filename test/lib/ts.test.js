d69a00983f77f0cab57a74d9461fc4247e544914
MaleDong <maledong_github@outlook.com>
2018-09-06 15:31:41 +0800

    chroe (typings): Remove 'ctx' in EggCookie's declaration and add a missing unit test
    1) The 'ctx' is exposed for test only instead of using it in the real
    'egg' env. So we can remove it so as not to show in the intellisense.
    
    2) Add a missing test to test what's happened when cookie's limit
    exceeded.

--- test/lib/ts.test.js
+++ test/lib/ts.test.js
@@ -4,16 +4,38 @@ const assert = require('assert');
 
 describe('start running ts file under the ts folder...', () => {
 
-  it('should run successfully and generate a json file to compare values', () => {
+  it('should run successfully and generate a json file to compare values', done => {
 
     // This will dynamically add cookieTest and have a run
-    require('ts-node').register({ typeCheck: true });
+    // We must specify the compiler mode in compilerOptions, because
+    // 'Promise' starts since es2015
+    require('ts-node').register({
+      typeCheck: true,
+      compilerOptions: {
+        target: 'es2015',
+        module: 'commonjs',
+        strict: true,
+        lib: [
+          'es2015',
+        ],
+      },
+    });
     const result = require('../fixtures/ts/cookiesTest');
 
     // Check one by one
     assert(result.test1.actualValue === 'bar');
     assert(result.test1.actualIndex === -1);
     assert(result.test2.actualValue === 'bar');
-    assert(undefined === result.test3.actualValue);
+    assert(result.test3.actualValue === undefined);
+
+    // Use promisible's then to make a call back and check values
+    result.test4.then(result => {
+      assert(result.name === 'foo');
+      assert(result.value === Buffer.allocUnsafe(4094).fill(49).toString());
+      assert(result.hasCtx);
+      assert(result.match);
+      // Call back to tell that unit test is over
+      done();
+    });
   });
 });
