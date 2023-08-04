f057f3f9b73410b56d7476e4115b07f34a632ef5
onlylovermb <general_up@163.com>
2023-08-04 11:58:27 +0800

    feat: support function to set domain

--- test/lib/cookie.test.js
+++ test/lib/cookie.test.js
@@ -10,25 +10,25 @@ function assertExceptionCheck(expectedMsg) {
 }
 describe('test/lib/cookie.test.js', () => {
   it('create cookies contains invalid string error should throw', () => {
-    assert.throws(() => new Cookie('中文', 'value'), assertExceptionCheck('argument name is invalid'));
-    assert.throws(() => new Cookie('name', '中文'), assertExceptionCheck('argument value is invalid'));
-    assert.throws(() => new Cookie('name', 'value', { path: '中文' }), assertExceptionCheck('argument option path is invalid'));
-    assert.throws(() => new Cookie('name', 'value', { domain: '中文' }), assertExceptionCheck('argument option domain is invalid'));
+    assert.throws(() => new Cookie({}, '中文', 'value'), assertExceptionCheck('argument name is invalid'));
+    assert.throws(() => new Cookie({}, 'name', '中文'), assertExceptionCheck('argument value is invalid'));
+    assert.throws(() => new Cookie({}, 'name', 'value', { path: '中文' }), assertExceptionCheck('argument option path is invalid'));
+    assert.throws(() => new Cookie({}, 'name', 'value', { domain: '中文' }), assertExceptionCheck('argument option domain is invalid'));
   });
 
   it('set expires to 0 if value not present', () => {
-    assert(new Cookie('name', null).attrs.expires.getTime() === 0);
+    assert(new Cookie({}, 'name', null).attrs.expires.getTime() === 0);
   });
 
   describe('toString()', () => {
     it('return name=vaule', () => {
-      assert(new Cookie('name', 'value').toString() === 'name=value');
+      assert(new Cookie({}, 'name', 'value').toString() === 'name=value');
     });
   });
 
   describe('toHeader()', () => {
     it('return name=vaule;params', () => {
-      assert(new Cookie('name', 'value', {
+      assert(new Cookie({}, 'name', 'value', {
         secure: true,
         maxAge: 1000,
         domain: 'eggjs.org',
@@ -37,15 +37,27 @@ describe('test/lib/cookie.test.js', () => {
       }).toHeader().match(/^name=value; path=\/; max-age=1; expires=(.*?)GMT; domain=eggjs\.org; secure; httponly$/));
     });
 
+    it('set domain when domain is a function', () => {
+      assert(new Cookie({
+        hostname: 'eggjs.org',
+      }, 'name', 'value', {
+        secure: true,
+        maxAge: 1000,
+        domain: ctx => ctx.hostname,
+        path: '/',
+        httpOnly: true,
+      }).toHeader().match(/^name=value; path=\/; max-age=1; expires=(.*?)GMT; domain=eggjs\.org; secure; httponly$/));
+    });
+
     it('donnot set path when set path to null', () => {
-      const header = new Cookie('name', 'value', {
+      const header = new Cookie({}, 'name', 'value', {
         path: null,
       }).toHeader();
       assert(!header.match(/path=/));
     });
 
     it('donnot set httponly when set httpOnly to false', () => {
-      const header = new Cookie('name', 'value', {
+      const header = new Cookie({}, 'name', 'value', {
         httpOnly: false,
       }).toHeader();
       assert(!header.match(/httponly/));
@@ -55,7 +67,7 @@ describe('test/lib/cookie.test.js', () => {
   describe('maxAge', () => {
     it('maxAge overwrite expires', () => {
       const expires = new Date('2020-01-01');
-      let header = new Cookie('name', 'value', {
+      let header = new Cookie({}, 'name', 'value', {
         secure: true,
         expires,
         domain: 'eggjs.org',
@@ -63,7 +75,7 @@ describe('test/lib/cookie.test.js', () => {
         httpOnly: true,
       }).toHeader();
       assert(header.match(/expires=Wed, 01 Jan 2020 00:00:00 GMT/));
-      header = new Cookie('name', 'value', {
+      header = new Cookie({}, 'name', 'value', {
         secure: true,
         maxAge: 1000,
         expires,
@@ -75,7 +87,7 @@ describe('test/lib/cookie.test.js', () => {
     });
 
     it('ignore maxage NaN', () => {
-      const header = new Cookie('name', 'value', {
+      const header = new Cookie({}, 'name', 'value', {
         secure: true,
         maxAge: 'session',
         domain: 'eggjs.org',
@@ -88,7 +100,7 @@ describe('test/lib/cookie.test.js', () => {
 
     it('ignore maxage 0', () => {
       // In previous implementations, maxAge = 0 was considered unnecessary to set this header
-      const header = new Cookie('name', 'value', {
+      const header = new Cookie({}, 'name', 'value', {
         secure: true,
         maxAge: 0,
         domain: 'eggjs.org',
@@ -102,13 +114,13 @@ describe('test/lib/cookie.test.js', () => {
 
   describe('sameSite', () => {
     it('should default to false', () => {
-      const cookie = new Cookie('foo', 'bar');
+      const cookie = new Cookie({}, 'foo', 'bar');
       assert.equal(cookie.attrs.sameSite, false);
     });
 
     it('should throw on invalid value', () => {
       assert.throws(() => {
-        new Cookie('foo', 'bar', { sameSite: 'foo' });
+        new Cookie({}, 'foo', 'bar', { sameSite: 'foo' });
       }, /argument option sameSite is invalid/);
     });
 
@@ -116,7 +128,7 @@ describe('test/lib/cookie.test.js', () => {
       it('should not add "samesite" attribute in header', () => {
         const falsyValues = [ false, 0, '', null, undefined, NaN ];
         falsyValues.forEach(falsy => {
-          const cookie = new Cookie('foo', 'bar', { sameSite: falsy });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: falsy });
           assert.ok(Object.is(cookie.attrs.sameSite, falsy));
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; httponly');
         });
@@ -125,7 +137,7 @@ describe('test/lib/cookie.test.js', () => {
 
     describe('when set to "true"', () => {
       it('should set "samesite=strict" attribute in header', () => {
-        const cookie = new Cookie('foo', 'bar', { sameSite: true });
+        const cookie = new Cookie({}, 'foo', 'bar', { sameSite: true });
         assert.equal(cookie.attrs.sameSite, true);
         assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=strict; httponly');
       });
@@ -134,11 +146,11 @@ describe('test/lib/cookie.test.js', () => {
     describe('when set to "none"', () => {
       it('should set "samesite=none" attribute in header', () => {
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'none' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'none' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=none; httponly');
         }
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'None' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'None' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=none; httponly');
         }
       });
@@ -147,11 +159,11 @@ describe('test/lib/cookie.test.js', () => {
     describe('when set to "lax"', () => {
       it('should set "samesite=lax" attribute in header', () => {
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'lax' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'lax' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=lax; httponly');
         }
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'Lax' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'Lax' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=lax; httponly');
         }
       });
@@ -160,11 +172,11 @@ describe('test/lib/cookie.test.js', () => {
     describe('when set to "strict"', () => {
       it('should set "samesite=strict" attribute in header', () => {
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'strict' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'strict' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=strict; httponly');
         }
         {
-          const cookie = new Cookie('foo', 'bar', { sameSite: 'Strict' });
+          const cookie = new Cookie({}, 'foo', 'bar', { sameSite: 'Strict' });
           assert.equal(cookie.toHeader(), 'foo=bar; path=/; samesite=strict; httponly');
         }
       });
